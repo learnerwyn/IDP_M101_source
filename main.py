86d183a4ab0d0b2ffa2b2df9fe9b058496adf432
SussyDriplord <jje36@cam.ac.uk>
2025-10-22 09:11:42 +0100

    layout of main.py

--- main.py
+++ main.py
@@ -11,3 +11,22 @@ from libs.VL53L0X.VL53L0X import VL53L0X
 # All the modules which have actions on the motors have arguments (motor_left, motor_right)
 motor_left = Motor(dirPin=4, PWMPin=5)  # Motor 1 is controlled from Motor Driv2 #1, which is on GP4/5
 motor_right = Motor(dirPin=4, PWMPin=5)
+
+
+# Activation mechanism via button on the physical bot
+
+
+# Start a timer for 4 ish minutes in order to complete parking sequence within time limit
+
+
+# Complete start sequence and move to zone 1
+
+
+# Complete default path repeatedly until QR code is found or time limit is reached
+
+
+# When a QR code is found, complete unloading sequence based on the QR code data
+# This includes a pickup mechanism, movement to the drop-off zone, and unloading mechanism
+
+
+# When the time limit is reached, complete parking sequence to park in the starting area
