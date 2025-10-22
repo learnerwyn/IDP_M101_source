d868ab5285e57b4fbf156495ce2e3b77e8b0193b
learnerwyn <15701571174@163.com>
2025-10-22 09:32:11 +0100

    complete the test file, ready to test when bot is ready

--- test_motion.py
+++ test_motion.py
@@ -3,6 +3,7 @@ from utime import sleep
 
 from motion_control import go_forward, go_back, turn_around, turn_left_90, turn_right_90, stop_the_car, adjust_to_left, adjust_to_right
 from motion_control import Motor
+from detection_module import qr_code_reader, distance_sensing, straight_line_detection
 
 # Input the pin numbers down here
 motor_left = Motor(dirPin=4, PWMPin=5)  # Motor 1 is controlled from Motor Driv2 #1, which is on GP4/5
@@ -23,16 +24,48 @@ def test2(motor_left, motor_right):
     sleep(5)
     turn_around(motor_left, motor_right)
     
-def test3():
+def test3(motor_left, motor_right):
     # test 3: adjust the car to the left and right
-    go_forward(motor_left, motor_right, 30)
+    go_forward(motor_left, motor_right, 50)
     sleep(3)
     adjust_to_left(motor_left, motor_right)
+    go_forward(motor_left, motor_right, 50)
     sleep(3)
     adjust_to_right(motor_left, motor_right)
+    go_forward(motor_left, motor_right, 50)
     sleep(3)
     stop_the_car(motor_left, motor_right)
     
+def test4(motor_left, motor_right):
+    # test 4: reading the qr code while moving, place the qr code a distance in front the bot before running this test
+    go_forward(motor_left, motor_right, 50)
+    code = None
+    while code == None:
+        code = qr_code_reader()
+        if code != None:
+            print(code)
+    stop_the_car(motor_left,motor_right)
+    
+def test5(motor_left, motor_right):
+    # test 5: reading distance while moving
+    go_forward(motor_left, motor_right, 50)
+    distance = 2000
+    while distance > 200 :
+        try:
+            distance = int(distance_sensing())
+        except:
+            distance = None
+        print(distance)
+    stop_the_car(motor_left,motor_right) # stop the car while the distance is 200 mm
+    
+def test6(motor_left, motor_right):
+    # test6: put the bot on a straight white line, test the straight line sensing
+    straight, temp = straight_line_detection()
+    if straight == True:
+        go_forward(motor_left, motor_right, 50)
+    while straight == True:
+        straight, temp = straight_line_detection()
+    stop_the_car(motor_left,motor_right) # stop the car while not following the straight line
 
 if __name__ == "__main__":
     test1()
